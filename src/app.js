447a54deb2e525d29a2fb2b059f99374d22a2f4d
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-08-25 16:15:08 +0100

    library list and imported old components

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 import reducers from './reducers';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText='Redux Tech Stack'/>
+        <LibraryList />
+      </View>
     </Provider>
   );
 };
